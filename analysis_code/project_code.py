dd6ffe288271e913fdc6fe9ada29e994bc001dd5
MSERG01 <savilabermudez1@babson.edu>
2023-03-21 01:35:43 -0400

    Update project_code.py

--- analysis_code/project_code.py
+++ analysis_code/project_code.py
@@ -1,67 +1,175 @@
+from mediawiki import MediaWiki
+from summa import summarizer, keywords
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.probability import FreqDist
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-# Need Function that takes URL as input and generates summary statsitics etc
-def wiki_summary(topic):
+
+# Need Function that takes URL as input and generates summary statistics etc
+def summarize_wiki(topic):
+    '''
+    Takes topic as a string and pulls data from MediaWiki 
+    1) Generates Wiki Summary using Media Wiki Function
+    2) Uses Summa to create summary and find Keywords from Content
+    3) Pull Links for later use
+    4) Store results in dictionary for later analysis
+
+    '''
+    # set site to URL in function in form 'http://.. and pass topic
+    site = MediaWiki(url='https://en.wikipedia.org/w/api.php')
+    page = MediaWiki().page(topic)
+
+    # generate summary using MediaWiki built in Function
+    summary_str = page.summary
+
+    # generate summary and keywords extraction using summa and store keywords in list
+    full_content = page.content
+    summa_summary = summarizer.summarize(full_content, ratio=0.2, words=None)
+    summa_keywords_list = keywords.keywords(full_content, split=True)
+    summa_keywords = keywords.keywords(full_content)
+
+    # extracting links
+    links = page.links
+
+    # store results
+    results = {'Wiki Summary': summary_str,
+               "Summa Summary": summa_summary,
+               "Summa Keywords as List": summa_keywords_list,
+               "Summa Keywords": summa_keywords,
+               "Links": links}
+    return results
+
+
+def process_words(words_list):
+    '''
+    Function used to clean up a list of words by removing stop words (the, of etc and ensure words are alphabetical)
+    2) Stem words for further accuracy
+    '''
+
+    stop_words = set(stopwords.words('english'))
+    filtered_words = []
+    for word in words_list:
+        if word.isalpha():
+            if word.lower() not in stop_words:
+                filtered_words.append(word)
+
+    # stem the words
+    stemmer = PorterStemmer()
+    stemmed_words = []
+    for word in filtered_words:
+        stemmed_words.append(stemmer.stem(word))
+
+    return stemmed_words
+
+
+def summary_statistics(dict, result_type):
+    '''
+    1) Uses nltk to tokenize sentences and words 
+    2) Calls remove_stop_words function to clean up words list 
+    3) Produce sumamry results from specified dictionary
+
+    '''
+    # Tokenize the text summary from previous function into sentences and words
+    sentences = sent_tokenize(dict[result_type])
+    words = word_tokenize(dict[result_type])
+
+    # Remove stopwords from list calling remove_stop_words() function
+    words = process_words(words)
+
+    # create dictionary of summary results
+    word_count = len(nltk.word_tokenize(dict[result_type]))
+    sentence_count = len(sentences)
+    unique_word_count = len(set(words))
+    avg_sentence_length = word_count / sentence_count
+
+    # Generate frequency distribution for words
+    word_freq_dist = FreqDist(words)
+    top_20_words = word_freq_dist.most_common(20)
+
+    result_statistics = {"Word Count": word_count,
+                         "Unique Word Count": unique_word_count,
+                         "Sentence Count": sentence_count,
+                         "Avg. Sentence Length": avg_sentence_length,
+                         "Top 20 Most Common Words": top_20_words}
+
+    return result_statistics
+
+
+def sentiment_analyzer(dict, result_type):
+    '''
+    generalized function for sentiment analysis built to take results from wiki_summary function
+
+
+    '''
+    # sentiment analysis
+    sentiment = SentimentIntensityAnalyzer()
+    sentiment_scores = sentiment.polarity_scores(dict[result_type])
+    return sentiment_scores
+
+
+def calculate_cosine_similarity(summary1, summary2):
+    '''
+    Will take two summary texts - from previous functions -- 
+    uses tfid vectorize from sklearn and calculates the cosine similarity using simialrity between vectors
+
+    '''
+    # tokenize summaries
+    tokenized_text1 = sent_tokenize(summary1)
+    tokenized_text2 = sent_tokenize(summary2)
+
+    # process the words
+    processed_summary1, processed_summary2 = "".join(process_words(
+        tokenized_text1)), " ".join(process_words(tokenized_text2))
+
+    # Vectorize the processed text
+    vectorize = TfidfVectorizer()
+    vectorized_summary1 = vectorize.fit_transform([processed_summary1])
+    vectorized_summary2 = vectorize.fit_transform([processed_summary2])
+
+    # calculate cosine simialrity between vectors
+    cosine_similarity = cosine_similarity(
+        vectorized_summary1, vectorized_summary2)
+
+    return cosine_similarity
+
+
+def main():
+
+    # from nltk.stem import PorterStemmer
+
     # Import required libraries
     # Must install
     # pip install mediawiki.
     # pip install scikit-learn
     # pip install summa
-    from mediawiki import MediaWiki
-    import nltk
-    from nltk.tokenize import sent_tokenize, word_tokenize
-    from nltk.corpus import stopwords
-    from nltk.stem import PorterStemmer
-    from nltk.sentiment.vader import SentimentIntensityAnalyzer
-    from sklearn.metrics.pairwise import cosine_similarity
-    from sklearn.feature_extraction.text import TfidfVectorizer
-    from summa.summarizer import summarize
-    # set site to URL in function in form 'http://
-    site = MediaWiki(url='https://en.wikipedia.org/w/api.php')
-    page = MediaWiki().page(topic)
-    # generate summary in str for topic (given input)
-    summary_str = page.summary
-    summa_summary = summarize(page, ratio=0.2, words=None)
-    # compute summary statistics
-    sentences = sent_tokenize(summary_str)
-    word_count = len(nltk.word_tokenize(summary_str))
-    sentence_count = len(sentences)
-    avg_sentence_length = word_count / sentence_count
-    # sentiment analysis
-    sid = SentimentIntensityAnalyzer()
-    sentiment_score = sid.polarity_scores(summary_str)\
-        # extracting links
-    links = page.links
-    # keyword extraction
-    stop_words = set(stopwords.words('english)'))
-    ps = PorterStemmer()
-    tokenized_words = [ps.stem(word.lower()) for word in word_tokenize(
-        summary_str) if word.lower() not in stop_words and word.isalpha()]
-    tfid_matrix = TfidfVectorizer().fit_transform(tokenized_words)
-    feature_names = TfidfVectorizer().get_feature_names()
-    # store results
-    results = [summary_str, summa_summary, sentiment_score, word_count,
-               sentence_count, avg_sentence_length]
-    # return statistics
-    return results
+    # pip instal nltk
 
-    # print statistics
-    # for i in range(len(results)):
-    #     print(f"{results[i]}")
-    # print(summary_str)
-    # print(f"Sentiment Score: {score}")
-    # print(f"Word count: {word_count}")
-    # print(f"Sentence Count: {sentence_count}")
-    # print(f"Average Sentence Length: {avg_sentence_length}")
+    topics = {'svb': "Collapse of Silicon Valley Bank",
+              'enron': 'Enron Scandal',
+              'lehman': 'Bankruptcy of Lehman Brothers',
+              'ftx': 'Bankruptcy of FTX',
+              'wework': 'WeWork',
+              'theranos': 'Theranos'
+              }
 
+    # Mock Analysis for SVB
+    svb = summarize_wiki(topics['svb'])
+    svb_summary_stats = summary_statistics(svb, "Wiki Summary")
+    print(svb_summary_stats)
+    print(sentiment_analyzer(svb, "Wiki Summary"))
+    print(sentiment_analyzer(svb, "Summa Summary"))
+    print(sentiment_analyzer(svb, "Summa Keywords"))
 
-def main():
-    # results = [summary_str, senitment_score, word_count,sentence_count, avg_sentence_length]
-    svb = wiki_summary('Collapse of Silicon Valley Bank')
-    enron = wiki_summary('Enron scandal')
-    lehman = wiki_summary('Bankruptcy of Lehman Brothers')
-    ftx = wiki_summary('Bankruptcy of FTX')
-    wework = wiki_summary('WeWork')
-    theranos = wiki_summary('Theranos')
+    # enron = summarize_wiki('Enron scandal')
+    # lehman = summarize_wiki('Bankruptcy of Lehman Brothers')
+    # ftx = summarize_wiki('Bankruptcy of FTX')
+    # wework = summarize_wiki('WeWork')
+    # theranos = summarize_wiki('Theranos')
 
 
 if __name__ == "__main__":
